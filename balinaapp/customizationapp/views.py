ab9733d8d26d5d3053e9e0ee60285a84d8043d68
ahmetyasinc <ahmetyasinc26@gmail.com>
2024-12-06 18:00:44 +0300

    Yeni bir başlangıç

---
+++ balinaapp/customizationapp/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+def indicator(request):
+
+    return render(request, "customizationapp/indicator.html")
---
+++ balinaapp/homeapp/static/homeapp/js/leftbar.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const sidebar = document.getElementById("sidebar");
+    const toggleBtn = document.getElementById("toggleSidebar");
+
+    toggleBtn.addEventListener("click", function () {
+        sidebar.classList.toggle("open");
+    });
+});
