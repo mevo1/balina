b61a65bde51aa1cb6c5db4861677c95258a748fb
ahmetyasinc <ahmetyasinc26@gmail.com>
2024-12-10 15:16:03 +0300

    İndikatör sayfası versiyon 1

--- balinaapp/homeapp/static/homeapp/js/leftbar.js
+++ balinaapp/homeapp/static/homeapp/js/leftbar.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const sidebar = document.getElementById("sidebar");
-    const toggleBtn = document.getElementById("toggleSidebar");
+    const sidebar = document.getElementById("sidebar-left");
+    const toggleBtn = document.getElementById("toggleSidebar-left");
 
     toggleBtn.addEventListener("click", function () {
         sidebar.classList.toggle("open");
